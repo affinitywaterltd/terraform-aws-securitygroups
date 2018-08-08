38e2aafef8712f850f038668b3bd18bb7c708cca
Ellis <tim.ellis@affinitywater.co.uk>
2018-08-08 10:30:38 +0100

    Correct CIDr range to list

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ resource "aws_security_group_rule" "ping" {
   from_port   = -1
   to_port     = -1
   protocol    = "icmp"
-  cidr_blocks = "10.0.0.0/8"
+  cidr_blocks = ["10.0.0.0/8"]
 
   security_group_id = "${aws_security_group.admin_sg.id}"
 }
