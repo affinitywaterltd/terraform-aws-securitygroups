673dbc52fef1eadd1115c6281fd75b23e0cae0f5
Ellis <tim.ellis@affinitywater.co.uk>
2018-08-10 15:27:23 +0100

    .

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+# Outbound rule!
+
+resource "aws_security_group_rule" "outbound" {
+  type                     = "egress"
+  from_port                = -1
+  to_port                  = -1
+  protocol                 = 01
+  cidr_blocks              = ["0.0.0.0/0"]
+
+  security_group_id = "${aws_security_group.admin_sg.id}"
+}
+
 # Admin SG
 #
 #
