126038cf9992d2de5ed1f0bd5aab5481a7fe39ec
Ellis <tim.ellis@affinitywater.co.uk>
2018-09-12 13:41:11 +0100

    Added Citrix SG

--- main.tf
+++ main.tf
@@ -194,3 +194,193 @@ resource "aws_security_group_rule" "HTTPS_Internal" {
 
   security_group_id = "${aws_security_group.open_http_https_sg.id}"
 }
+
+
+## Citrix
+
+resource "aws_security_group" "citrix_sg" {
+  name        = "Core_Citrix"
+  description = "For Citrix created machines"
+  vpc_id      = "${var.vpc_id}"
+}
+
+resource "aws_security_group_rule" "8082_Internal" {
+  type        = "ingress"
+  from_port   = 8082
+  to_port     = 8083
+  protocol    = "tcp"
+  cidr_blocks = ["10.0.0.0/8"]
+
+  security_group_id = "${aws_security_group.citrix_sg.id}"
+}
+
+
+resource "aws_security_group_rule" "80_Internal" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["10.0.0.0/8"]
+
+  security_group_id = "${aws_security_group.citrix_sg.id}"
+}
+
+resource "aws_security_group_rule" "443_Internal" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["10.0.0.0/8"]
+
+  security_group_id = "${aws_security_group.citrix_sg.id}"
+}
+
+resource "aws_security_group_rule" "1494_Internal" {
+  type        = "ingress"
+  from_port   = 1494
+  to_port     = 1494
+  protocol    = "tcp"
+  cidr_blocks = ["10.0.0.0/8"]
+
+  security_group_id = "${aws_security_group.citrix_sg.id}"
+}
+
+resource "aws_security_group_rule" "2598_Internal" {
+  type        = "ingress"
+  from_port   = 2598
+  to_port     = 2598
+  protocol    = "tcp"
+  cidr_blocks = ["10.0.0.0/8"]
+
+  security_group_id = "${aws_security_group.citrix_sg.id}"
+}
+
+resource "aws_security_group_rule" "8008_Internal" {
+  type        = "ingress"
+  from_port   = 8008
+  to_port     = 8008
+  protocol    = "tcp"
+  cidr_blocks = ["10.0.0.0/8"]
+
+  security_group_id = "${aws_security_group.citrix_sg.id}"
+}
+
+resource "aws_security_group_rule" "2512_Internal" {
+  type        = "ingress"
+  from_port   = 2512
+  to_port     = 2513
+  protocol    = "tcp"
+  cidr_blocks = ["10.0.0.0/8"]
+
+  security_group_id = "${aws_security_group.citrix_sg.id}"
+}
+
+resource "aws_security_group_rule" "8080_Internal" {
+  type        = "ingress"
+  from_port   = 8080
+  to_port     = 8080
+  protocol    = "tcp"
+  cidr_blocks = ["10.0.0.0/8"]
+
+  security_group_id = "${aws_security_group.citrix_sg.id}"
+}
+
+resource "aws_security_group_rule" "upd_range_Internal1" {
+  type        = "ingress"
+  from_port   = 16500
+  to_port     = 16509
+  protocol    = "udp"
+  cidr_blocks = ["10.0.0.0/8"]
+
+  security_group_id = "${aws_security_group.citrix_sg.id}"
+}
+
+resource "aws_security_group_rule" "upd_range_Internal2" {
+  type        = "ingress"
+  from_port   = 49152
+  to_port     = 65535
+  protocol    = "udp"
+  cidr_blocks = ["10.0.0.0/8"]
+
+  security_group_id = "${aws_security_group.citrix_sg.id}"
+}
+
+resource "aws_security_group_rule" "upd_9_Internal" {
+  type        = "ingress"
+  from_port   = 9
+  to_port     = 9
+  protocol    = "udp"
+  cidr_blocks = ["10.0.0.0/8"]
+
+  security_group_id = "${aws_security_group.citrix_sg.id}"
+}
+
+resource "aws_security_group_rule" "8100_Internal" {
+  type        = "ingress"
+  from_port   = 8100
+  to_port     = 8100
+  protocol    = "tcp"
+  cidr_blocks = ["10.0.0.0/8"]
+
+  security_group_id = "${aws_security_group.citrix_sg.id}"
+}
+
+resource "aws_security_group_rule" "1433_Internal" {
+  type        = "ingress"
+  from_port   = 1433
+  to_port     = 1434
+  protocol    = "tcp"
+  cidr_blocks = ["10.0.0.0/8"]
+
+  security_group_id = "${aws_security_group.citrix_sg.id}"
+}
+
+resource "aws_security_group_rule" "135_citrix_delivery" {
+  type        = "ingress"
+  from_port   = 135
+  to_port     = 135
+  protocol    = "tcp"
+  cidr_blocks = ["10.31.103.203/32", "10.31.106.65/32"]
+
+  security_group_id = "${aws_security_group.citrix_sg.id}"
+}
+
+resource "aws_security_group_rule" "5985_citrix_director" {
+  type        = "ingress"
+  from_port   = 5985
+  to_port     = 5985
+  protocol    = "tcp"
+  cidr_blocks = ["10.31.100.33/32", "10.31.104.120/32"]
+
+  security_group_id = "${aws_security_group.citrix_sg.id}"
+}
+
+resource "aws_security_group_rule" "3389_Internal" {
+  type        = "ingress"
+  from_port   = 3389
+  to_port     = 3389
+  protocol    = "tcp"
+  cidr_blocks = ["10.0.0.0/8"]
+
+  security_group_id = "${aws_security_group.citrix_sg.id}"
+}
+
+resource "aws_security_group_rule" "389_citrix_delivery" {
+  type        = "ingress"
+  from_port   = 389
+  to_port     = 389
+  protocol    = "tcp"
+  cidr_blocks = ["10.31.103.203/32", "10.31.106.65/32"]
+
+  security_group_id = "${aws_security_group.citrix_sg.id}"
+}
+
+resource "aws_security_group_rule" "2598_Internal" {
+  type        = "ingress"
+  from_port   = 2598
+  to_port     = 2598
+  protocol    = "tcp"
+  cidr_blocks = ["10.0.0.0/8"]
+
+  security_group_id = "${aws_security_group.citrix_sg.id}"
+}
